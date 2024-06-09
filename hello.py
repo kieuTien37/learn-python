0ff45978c64f0759cb2e3ea157680d984ce53d14
kieunt.2501@outlook.com <you@example.com>
2024-06-09 12:03:01 +0700

    Add a heading

---
+++ hello.py
@@ -0,0 +1,14 @@
+name = input("Name: ")
+print("Hello, " + name)
+
+ten =["Hoa", "Ha"]
+print(ten[0])
+
+n = int(input("Number: "))
+if n > 0:
+    print ("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
+
